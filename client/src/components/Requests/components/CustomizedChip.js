4ecfb06216dd9e44998aa84cf42874f231f69904
SimonGodefroid <simon.godefroid@gmail.com>
2018-05-21 21:53:28 +0200

    Change Landing Page Style

--- client/src/components/Requests/components/CustomizedChip.js
+++ client/src/components/Requests/components/CustomizedChip.js
@@ -5,44 +5,47 @@ import Avatar from 'material-ui/Avatar';
 import Chip from 'material-ui/Chip';
 import { PulseLoader } from 'halogenium';
 
-const styles = theme => ({
-  root: {
-    display: 'flex',
-    justifyContent: 'center',
-    flexWrap: 'wrap',
-  },
-  chip: {
-    margin: theme.spacing.unit,
-    backgroudColor:'white',
-  },
+const styles = (theme) => ({
+	root: {
+		display: 'flex',
+		justifyContent: 'center',
+		flexWrap: 'wrap',
+	},
+	chip: {
+		margin: theme.spacing.unit,
+		backgroudColor: 'white',
+	},
 });
 
+const handleClick = () => {
+	alert('You clicked the Chip.'); // eslint-disable-line no-alert
+};
 
- const handleClick = () => {
-  alert('You clicked the Chip.'); // eslint-disable-line no-alert
-}
-
-const Chips = (props) =>{
-  const { classes } = props;
-  if(props.user){
-    return (
-      <div className={classes.root}>
-        <Chip
-          avatar={<Avatar src={props.user.avatar} alt={`${props.user.username} profile`}/>}
-          label={`${props.user.username.substring(0, 5)}...`}
-          onClick={handleClick}
-          className={classes.chip}
-        />
-      </div>
-    );
-  }
-else{
-  return <PulseLoader/>
-}
-}
+const Chips = (props) => {
+	const { classes } = props;
+	if (props.user) {
+		return (
+			<div className={classes.root}>
+				<Chip
+					avatar={
+						<Avatar
+							src={props.user.avatar}
+							alt={`${props.user.username} profile`}
+						/>
+					}
+					label={`${props.user.username ? props.user.username.substring(0, 5): `user_${props.user._id}`}...`}
+					onClick={handleClick}
+					className={classes.chip}
+				/>
+			</div>
+		);
+	} else {
+		return <PulseLoader />;
+	}
+};
 
 Chips.propTypes = {
-  classes: PropTypes.object.isRequired,
+	classes: PropTypes.object.isRequired,
 };
 
 export default withStyles(styles)(Chips);
